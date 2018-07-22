6597f97678f4c0576556db548f54a912153e4e17
330mlcc <mlcc330@hotmail.com>
2018-07-22 16:05:02 +0800

    update project

---
+++ src/scanless/scanless/scanners/scanless.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python  
+# -*- coding: utf-8 -*-
+
+""" 
+@version: v1.0 
+@author: 330mlcc 
+@Software: PyCharm
+@license: Apache Licence  
+@Email   : mlcc330@hotmail.com
+@contact: 3323202070@qq.com
+@site:  
+@software: PyCharm 
+@file: scanless.py
+@time: 18-7-22 下午4:03 
+Description: 
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+    pass 
+    
+    
